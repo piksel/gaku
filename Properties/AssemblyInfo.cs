f1fae5e3551c85c0f32a905adf2abdd6b716141c
nils måsén <nils@piksel.se>
2016-09-07 00:27:43 +0200

    Bump version for 3rd beta release

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.6.16250.598")]
-[assembly: AssemblyFileVersion("0.6.16250.598")]
+[assembly: AssemblyVersion("0.7.16250.681")]
+[assembly: AssemblyFileVersion("0.7.16250.681")]
